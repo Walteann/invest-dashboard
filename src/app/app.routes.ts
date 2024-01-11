a9d0abfabef74e6a71e3815f34b4d70392919776
Walteann <walteann3@gmail.com>
2024-01-10 23:44:01 -0300

    feat: dashboard cards

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+	{
+		path: '',
+		loadComponent: () => import('./pages/dashboard/dashboard.component').then(c => c.DashboardComponent)
+	},
+	{
+		path: '',
+		redirectTo: '',
+		pathMatch: 'full'
+	}
+];
