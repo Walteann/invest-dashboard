67375ad68824288d3b34996c48ab18b5db5be43f
Walteann <walteann3@gmail.com>
2024-01-19 22:29:25 -0300

    feat: lista de transacoes recentes

--- src/app/pages/dashboard/components/recents-transactions/recents-transactions.component.ts
+++ src/app/pages/dashboard/components/recents-transactions/recents-transactions.component.ts
@@ -3,6 +3,7 @@ import { Component, OnInit } from '@angular/core';
 interface RecentTrasanction {
 	icon: string;
 	transactionName: string;
+	className: string;
 	date: string;
 	flagCart: string;
 	cartNumber: string;
@@ -23,12 +24,53 @@ export class RecentsTransactionsComponent {
 		{
 			icon: 'headphones',
 			transactionName: 'Música',
+			className: 'bg-purple-primary',
 			date: '20/11/2021',
 			flagCart: 'visa.png',
 			cartNumber: '*2757',
 			typeTransaction: 'Cartão de Crédito',
 			value: 19.90,
-		}
+		},
+		{
+			icon: 'shopping_bag',
+			transactionName: 'Shopping',
+			className: 'bg-primary',
+			date: '19/11/2021',
+			flagCart: 'visa.png',
+			cartNumber: '*2757',
+			typeTransaction: 'Cartão de Crédito',
+			value: 19.90,
+		},
+		{
+			icon: 'restaurant',
+			transactionName: 'Restaurante',
+			className: 'bg-danger-light',
+			date: '20/11/2021',
+			flagCart: 'master_card.png',
+			cartNumber: '*4452',
+			typeTransaction: 'Cartão de Crédito',
+			value: 15.90,
+		},
+		{
+			icon: 'sports_esports',
+			transactionName: 'Games',
+			className: 'bg-danger-light',
+			date: '15/11/2021',
+			flagCart: 'visa.png',
+			cartNumber: '*2757',
+			typeTransaction: 'Cartão de Crédito',
+			value: 79.90,
+		},
+		{
+			icon: 'medication',
+			transactionName: 'Farmácia',
+			className: 'bg-danger-light',
+			date: '15/11/2021',
+			flagCart: 'visa.png',
+			cartNumber: '*2757',
+			typeTransaction: 'Cartão de Crédito',
+			value: 79.90,
+		},
 	];
 
 }
