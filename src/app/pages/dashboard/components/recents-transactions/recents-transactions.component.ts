76195f7764ad2d65f3c0b1a247dc4948057821b0
Walteann <walteann3@gmail.com>
2024-01-21 20:41:39 -0300

    feat: finalizando css de transacoes recentes

--- src/app/pages/dashboard/components/recents-transactions/recents-transactions.component.ts
+++ src/app/pages/dashboard/components/recents-transactions/recents-transactions.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 
 interface RecentTrasanction {
 	icon: string;
+	iconClass: string;
 	transactionName: string;
 	className: string;
 	date: string;
@@ -9,6 +10,7 @@ interface RecentTrasanction {
 	cartNumber: string;
 	typeTransaction: string;
 	value: number;
+	flagCartClass: string;
 }
 
 @Component({
@@ -23,50 +25,60 @@ export class RecentsTransactionsComponent {
 	recentsTransactions: RecentTrasanction[] = [
 		{
 			icon: 'headphones',
+			iconClass: 'purple',
 			transactionName: 'Música',
-			className: 'bg-purple-primary',
+			className: 'bg-purple-light',
 			date: '20/11/2021',
 			flagCart: 'visa.png',
+			flagCartClass: 'bg-danger',
 			cartNumber: '*2757',
 			typeTransaction: 'Cartão de Crédito',
 			value: 19.90,
 		},
 		{
 			icon: 'shopping_bag',
+			iconClass: 'purple',
 			transactionName: 'Shopping',
-			className: 'bg-primary',
+			className: 'bg-purple-light',
 			date: '19/11/2021',
 			flagCart: 'visa.png',
+			flagCartClass: 'bg-primary',
 			cartNumber: '*2757',
 			typeTransaction: 'Cartão de Crédito',
 			value: 19.90,
 		},
 		{
 			icon: 'restaurant',
+			iconClass: 'success',
 			transactionName: 'Restaurante',
-			className: 'bg-danger-light',
+			className: 'bg-success-light',
 			date: '20/11/2021',
 			flagCart: 'master_card.png',
+			flagCartClass: 'bg-dark',
 			cartNumber: '*4452',
 			typeTransaction: 'Cartão de Crédito',
 			value: 15.90,
 		},
 		{
 			icon: 'sports_esports',
+			iconClass: 'danger',
 			transactionName: 'Games',
 			className: 'bg-danger-light',
 			date: '15/11/2021',
 			flagCart: 'visa.png',
+			flagCartClass: 'bg-danger',
 			cartNumber: '*2757',
 			typeTransaction: 'Cartão de Crédito',
 			value: 79.90,
 		},
 		{
 			icon: 'medication',
+			iconClass: 'danger',
 			transactionName: 'Farmácia',
 			className: 'bg-danger-light',
 			date: '15/11/2021',
 			flagCart: 'visa.png',
+			flagCartClass: 'bg-danger',
 			cartNumber: '*2757',
 			typeTransaction: 'Cartão de Crédito',
 			value: 79.90,
